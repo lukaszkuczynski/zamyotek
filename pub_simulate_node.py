73997f7ec4c474af0fa57ced8e7fe46e223dedac
lukaszkuczynski <kuczynskilukasz@gmail>
2021-10-30 13:07:19 -0400

    Simulate values publisher node

---
+++ pub_simulate_node.py
@@ -0,0 +1,30 @@
+from mqtt_node import MqttNode
+import time
+from argparse import ArgumentParser
+
+class SimPubNode(MqttNode):
+
+    def __init__(self, topic, key, value):
+        nodename = "simulate_pub_for_"+topic
+        super().__init__(nodename, topic, "")
+        self.key = key
+        self.value = value
+        self.__loop()
+
+    def __loop(self):
+        while True:
+            value = self.value
+            msg = {self.key: value}
+            self.send(msg)
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("--value", default=1)
+    parser.add_argument("--topic", default="simulate")
+    args = parser.parse_args()
+    value = args.value
+    if value.isnumeric():
+        value = float(value)
+    sim = SimPubNode(args.topic, "msg", value)
