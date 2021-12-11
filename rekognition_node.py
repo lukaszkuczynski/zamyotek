b0d922ed68f41b98a517d19b9d4955645032cd42
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-12-11 15:10:36 +0100

    Recognition implemented.

--- rekognition_node.py
+++ rekognition_node.py
@@ -1,5 +1,6 @@
 from mqtt_node import MqttNode
 import json
+import boto3
 
 TOPIC_NOTIFY_PHOTO_TAKEN = "/notify/photo"
 TOPIC_NOTIFY_OBJECT_RECOGNIZED = "/notify/object_recognized"
@@ -12,13 +13,22 @@ class RekognitionNode(MqttNode):
         )
 
     def recognize_from_path(self, picture_path):
-        return "banana"
+        client = boto3.client("rekognition")
+        with open(picture_path, "rb") as image:
+            response = client.detect_labels(Image={"Bytes": image.read()})
+        labels_and_confidence = [
+            (label["Name"], label["Confidence"]) for label in response["Labels"]
+        ]
+        self.logger.debug(labels_and_confidence)
+        return labels_and_confidence
 
     def on_message(self, client, userdata, msg):
         camera_photo_msg = json.loads(msg.payload)
         picture_path = camera_photo_msg["msg"]
         self.logger.info("Received picture on %s", picture_path)
-        object_class = self.recognize_from_path(picture_path)
+        labels_and_confidence = self.recognize_from_path(picture_path)
+        self.logger.info("Choosing label - blunt shot for the 1st one")
+        object_class = labels_and_confidence[0][0]
         self.send(object_class)
 
 
--- rekognition_poc.py
+++ rekognition_poc.py
@@ -1,22 +1,19 @@
 import boto3
 
+
 def detect_labels_local_file(photo):
+    client = boto3.client("rekognition")
+    with open(photo, "rb") as image:
+        response = client.detect_labels(Image={"Bytes": image.read()})
+    print("Detected labels in " + photo)
+    for label in response["Labels"]:
+        print(label["Name"] + " : " + str(label["Confidence"]))
+    return len(response["Labels"])
 
-
-    client=boto3.client('rekognition')
-   
-    with open(photo, 'rb') as image:
-        response = client.detect_labels(Image={'Bytes': image.read()})
-        
-    print('Detected labels in ' + photo)    
-    for label in response['Labels']:
-        print (label['Name'] + ' : ' + str(label['Confidence']))
-
-    return len(response['Labels'])
 
 def main():
-    photo='fritzing_bb.png'
-    label_count=detect_labels_local_file(photo)
+    photo = "fritzing_bb.png"
+    label_count = detect_labels_local_file(photo)
     print("Labels detected: " + str(label_count))
 
 
